719b30d428a50e8b9560e9bb938e12a96281be9a
Marco Bellomo <marco@lotrek.it>
2018-01-10 19:22:04 +0100

    traceroute implemented

--- src/app/googlemap/googlemap.component.ts
+++ src/app/googlemap/googlemap.component.ts
@@ -17,51 +17,62 @@ export class GooglemapComponent implements OnInit {
   public marker =[];
   public destination=[];
   vehicles = [];
+  public directionsService;
+  public directionsDisplay;
   constructor(service: GooglemapService) { 
    
     this.vehicles= service.getVehicle();
+    this.directionsService=new google.maps.DirectionsService();
+    this.directionsDisplay=new google.maps.DirectionsRenderer();
   }
   
   ngOnInit() {
+    
     this.destination[1]=new google.maps.LatLng(43.9320643,10.932747);
     this.map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 10, lng: 20},
       zoom: 15
     });
-    this.setPoint(43.9320643,10.932747);
+    
     this.setCurrentPosition();
   }
 
   private setCurrentPosition() {
  
    if (navigator.geolocation) {
-     alert('1');
-      navigator.geolocation.getCurrentPosition((position)=> {
+     
+      navigator.geolocation.watchPosition((position)=> {
         var pos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
         console.log(pos);
-        alert('2');
+        
         this.map.setCenter(pos);
-        alert('3');
+        this.setPoint(pos.lat,pos.lng);
       });
     } else {
       // Browser doesn't support Geolocation
      // handleLocationError(false, infoWindow, map.getCenter());
     }
   }
-  /*private traceRoute(a)
+  private traceRoute(a)
   {
     var request= {
       origin:this.destination[1],
       destination:this.destination[a],
-      travelmode:"driving"
+      travelMode:"DRIVING"
     };
-    directionsService.route(request,function(result,status){
+    console.log(request);
+    this.directionsDisplay.setMap(this.map);
+    this.directionsService.route(request,(result,status)=>{
       console.log(result,status);
+      if (status === google.maps.DirectionsStatus.OK) {
+        this.directionsDisplay.setDirections(result);
+    }
+      
     })
-  }*/
+  }
   private setPoint(ltg,lng){
     this.num=this.num+1;
     var a=this.num;
@@ -74,7 +85,7 @@ export class GooglemapComponent implements OnInit {
     this.marker[a].setMap(this.map);
     if (a>1)
     {
-      google.maps.event.addDomListener(this.marker[a], 'click', function() {
+      google.maps.event.addDomListener(this.marker[a], 'click', ()=> {
         this.traceRoute(a);
        });
     }
